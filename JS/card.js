9c43bd88607da0e8f58de38067b92dc51489ebd3
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2023-03-06 10:19:07 +0100

    progressing

--- JS/card.js
+++ JS/card.js
@@ -1,9 +1,15 @@
-let button = document.querySelector("#confirm");
-let cardName = document.querySelector("#cardholder-name");
+let form = document.getElementById("myForm");
+let cardName = document.getElementById("cardholder-name");
 let cardNumber = document.querySelector("#card-number");
 let expMonth = document.querySelector("#card-exp-month");
 let expYear = document.querySelector("#card-exp-year");
 let cvc = document.querySelector("#card-cvc");
+let errorText = document.querySelector("small");
+let cardNames = document.querySelector("#cardholder-name");
+let cardNumbers = document.querySelector("#card-number");
+let expMonths = document.querySelector("#card-exp-month");
+let expYears = document.querySelector("#card-exp-year");
+let cvcs = document.querySelector("#card-cvc");
 let names = document.querySelector(".names");
 let details = document.querySelector(".detail");
 let cardBack = document.querySelector(".card_back");
@@ -11,83 +17,107 @@ let month = document.querySelector(".month");
 let year = document.querySelector(".year");
 let rightSide = document.querySelector(".right");
 let complete = document.querySelector(".complete");
-let small = document.querySelector("small")
-let nameValue = cardName.value;
-//console.log(nameValue + "works")
-let cardNumberValue = cardNumber.value;
-let cvcValue = cvc.value;
-let monthValue = expMonth.value;
-let yearValue = expYear.value;
+form.addEventListener("submit", function () {
+  e.preventDefault()
+  //validate();
+   cardNameValidation()
+    cardNumberValidation()
+    expMonthValidation()
+    expYearValidation()
+    cardCVCValidation() 
+    inputing()
+        completing()
+});
 
-button.addEventListener("click", function() {
-  inputing()
-  //console.log("works")
-})
- function inputing() {
-  let updateName = (names.textContent = nameValue);
-  console.log(updateName + "Works")
-   let updateCardNumber = (details.textContent = cardNumberValue);
-  let updateCVC = (cardBack.textContent = cvcValue);
-  let updateMonth = (month.textContent = monthValue);
-  let updateYear = (year.textContent = yearValue);
+function validation() {
+  cardNameValidation() ||
+    cardNumberValidation() ||
+    expMonthValidation() ||
+    expYearValidation() ||
+    cardCVCValidation();
 }
-/* function checkCardNumber() {
- if (numberValue == "") {
-    showError(cardNumber, "card number cannot be blank")
+function validate() {
+  if (
+    cardNameValidation() &&
+    cardNumberValidation() &&
+    expMonthValidation() &&
+    expYearValidation() &&
+    cardCVCValidation() == true
+  ) {
+    inputing();
+    completing();
+  }
 }
-else {
-    showSuccess(cardNumber)
-}
-}
-function checkName() {
-    if (naValue == "") {
-        showError(name , "card name cannot be blank")
-    }
-    else {
-        showSuccess(expMonth)
-    }
-}
-function checkMonth() {
-    if (monValue == "") {
-       showError(expMonth, "Month cannot be blank")
-   }
-   else {
-       showSuccess(expMonth)
-   }
-   }
-  
-   function checkYear() {
-    if ( yeValue== "") {
-       showError(expYear, "card Year cannot be blank")
-   }
-   else {
-       showSuccess(expYear)
-   }
-   }
-   function checkCVC() {
-    if ( CVCValue=="") {
-       showError(cvc, "card cvc cannot be blank")
-   }
-   else {
-       showSuccess(cvc)
-   }
-   }
 
-function showError(input, message) {
-    input.classList.remove('success');
-    input.classList.add('error');
-    //console.log('error')
-
-    // show the error message
-    small.textContent = message;
-};
-function showSuccess(input){
-    // get the form-field element
-
-    // remove the error class
-    input.classList.remove('error');
-    input.classList.add('success');
-
-    // hide the error message
-    small.textContent = '';
-} */
+function completing() {
+  rightSide.style.display = "none";
+  complete.style.display = "block";
+}
+function cardNameValidation() {
+  if (cardName.value == "") {
+    //errorText.textContent = "You must fill this field"
+    //errorText.style.color = "red"
+    cardName.style.borderColor = "red";
+  } else {
+    //errorText.textContent = "Congratulation"
+    //errorText.style.color = "green"
+    cardName.style.borderColor = "hsl(279, 6%, 55%)";
+  }
+}
+function cardNumberValidation() {
+  if (cardNumber.value == "") {
+    //  errorText.textContent = "You must fill this field"
+    //  errorText.style.color = "red"
+    cardNumber.style.borderColor = "red";
+  } else {
+    //  errorText.textContent = "Congratulation"
+    //  errorText.style.color = "green"
+    cardNumber.style.borderColor = "hsl(279, 6%, 55%)";
+  }
+}
+function expMonthValidation() {
+  if (expMonth.value == "") {
+    //errorText.textContent = "You must fill this field"
+    // errorText.style.color = "red"
+    expMonth.style.borderColor = "red";
+  } else if (expMonth.value.length > 2) {
+    expMonth.style.borderColor = "red";
+  } else {
+    // errorText.textContent = "Congratulation"
+    // errorText.style.color = "none"
+    expMonth.style.borderColor = "hsl(279, 6%, 55%)";
+  }
+}
+function expYearValidation() {
+  if (expYear.value == "") {
+    // errorText.textContent = "You must fill this field"
+    // errorText.style.color = "red"
+    expYear.style.borderColor = "red";
+  } else if (expYear.value.length > 2) {
+    expYear.style.borderColor = "red";
+  } else {
+    //errorText.textContent = "Congratulation"
+    //errorText.style.color = "none"
+    expYear.style.borderColor = "hsl(279, 6%, 55%)";
+  }
+}
+function cardCVCValidation() {
+  if (cvc.value == "") {
+    //errorText.textContent = "You must fill this field"
+    // errorText.style.color = "red"
+    cvc.style.borderColor = "red";
+  } else if (cvc.value.length > 3) {
+    cvc.style.borderColor = "red";
+  } else {
+    //  errorText.textContent = "Congratulation"
+    // errorText.style.color = "none"
+    cvc.style.borderColor = "hsl(279, 6%, 55%)";
+  }
+}
+function inputing() {
+  let updateName = (names.textContent = cardNames.value);
+  let updateCardNumber = (details.textContent = cardNumbers.value);
+  let updateCVC = (cardBack.textContent = cvcs.value);
+  let updateMonth = (month.textContent = expMonths.value);
+  let updateYear = (year.textContent = expYears.value);
+}
