030d0b9e2afaa1e3d6778655a50d908eb21ac220
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2023-03-08 16:13:54 +0100

    progessing 2.0

--- JS/card.js
+++ JS/card.js
@@ -17,101 +17,42 @@ let month = document.querySelector(".month");
 let year = document.querySelector(".year");
 let rightSide = document.querySelector(".right");
 let complete = document.querySelector(".complete");
-form.addEventListener("submit", function () {
-  e.preventDefault()
-  //validate();
-   cardNameValidation()
-    cardNumberValidation()
-    expMonthValidation()
-    expYearValidation()
-    cardCVCValidation() 
-    inputing()
-        completing()
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  Validation();
 });
 
-function validation() {
-  cardNameValidation() ||
-    cardNumberValidation() ||
-    expMonthValidation() ||
-    expYearValidation() ||
-    cardCVCValidation();
-}
-function validate() {
-  if (
-    cardNameValidation() &&
-    cardNumberValidation() &&
-    expMonthValidation() &&
-    expYearValidation() &&
-    cardCVCValidation() == true
-  ) {
-    inputing();
-    completing();
-  }
-}
-
-function completing() {
-  rightSide.style.display = "none";
-  complete.style.display = "block";
-}
-function cardNameValidation() {
+function Validation() {
   if (cardName.value == "") {
     //errorText.textContent = "You must fill this field"
     //errorText.style.color = "red"
     cardName.style.borderColor = "red";
-  } else {
-    //errorText.textContent = "Congratulation"
-    //errorText.style.color = "green"
-    cardName.style.borderColor = "hsl(279, 6%, 55%)";
-  }
-}
-function cardNumberValidation() {
-  if (cardNumber.value == "") {
+  } else if (cardNumber.value == "") {
     //  errorText.textContent = "You must fill this field"
     //  errorText.style.color = "red"
     cardNumber.style.borderColor = "red";
-  } else {
-    //  errorText.textContent = "Congratulation"
-    //  errorText.style.color = "green"
-    cardNumber.style.borderColor = "hsl(279, 6%, 55%)";
-  }
-}
-function expMonthValidation() {
-  if (expMonth.value == "") {
+  } else if (expMonth.value == "") {
     //errorText.textContent = "You must fill this field"
     // errorText.style.color = "red"
     expMonth.style.borderColor = "red";
   } else if (expMonth.value.length > 2) {
     expMonth.style.borderColor = "red";
-  } else {
-    // errorText.textContent = "Congratulation"
-    // errorText.style.color = "none"
-    expMonth.style.borderColor = "hsl(279, 6%, 55%)";
-  }
-}
-function expYearValidation() {
-  if (expYear.value == "") {
+  } else if (expYear.value == "") {
     // errorText.textContent = "You must fill this field"
     // errorText.style.color = "red"
     expYear.style.borderColor = "red";
   } else if (expYear.value.length > 2) {
     expYear.style.borderColor = "red";
-  } else {
-    //errorText.textContent = "Congratulation"
-    //errorText.style.color = "none"
-    expYear.style.borderColor = "hsl(279, 6%, 55%)";
-  }
-}
-function cardCVCValidation() {
-  if (cvc.value == "") {
+  } else if (cvc.value == "") {
     //errorText.textContent = "You must fill this field"
     // errorText.style.color = "red"
     cvc.style.borderColor = "red";
   } else if (cvc.value.length > 3) {
     cvc.style.borderColor = "red";
   } else {
-    //  errorText.textContent = "Congratulation"
-    // errorText.style.color = "none"
-    cvc.style.borderColor = "hsl(279, 6%, 55%)";
+    inputing();
+    completing();
   }
 }
 function inputing() {
@@ -120,4 +61,8 @@ function inputing() {
   let updateCVC = (cardBack.textContent = cvcs.value);
   let updateMonth = (month.textContent = expMonths.value);
   let updateYear = (year.textContent = expYears.value);
+}
+function completing() {
+  rightSide.style.display = "none";
+  complete.style.display = "block";
 }
